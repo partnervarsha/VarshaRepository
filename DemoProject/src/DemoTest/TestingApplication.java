104a6d3cbe01e990cc78fbea44a778d159036b4c
partnervarsha <partner.varsha.s@philips.com>
2024-06-21 18:36:42 +0530

    First Commit

---
+++ DemoProject/src/DemoTest/TestingApplication.java
@@ -0,0 +1,18 @@
+package DemoTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestingApplication {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		driver.close();
+		
+
+	}
+
+}
