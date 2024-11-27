b187dc4968b9c76d52d97fd20c2c7e4718033df0
320258581 <320258581@INGBTCPIC6VWM41.code1.emi.philips.com>
2024-11-27 15:15:37 +0530

    Changed the package name

--- DemoProject/src/DemoTest/TestScenario.java
+++ DemoProject/src/DemoTest/TestScenario.java
@@ -1,15 +1,16 @@
 package DemoTest;
 
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class cucumberTest {
+
+public class TestScenario {
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
 	WebDriver driver= new ChromeDriver();
 	driver.manage().window().maximize();
+	Thread.sleep(6000);
 	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 	driver.close();
 	
