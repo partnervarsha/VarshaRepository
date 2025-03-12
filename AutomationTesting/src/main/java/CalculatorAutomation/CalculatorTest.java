7183ef33a2f82677389f436a7981e084f4b7b104
320258581 <320258581@INGBTCPIC6VWM41.code1.emi.philips.com>
2025-03-12 14:13:11 +0530

    Code Changes

--- AutomationTesting/src/main/java/CalculatorAutomation/CalculatorTest.java
+++ AutomationTesting/src/main/java/CalculatorAutomation/CalculatorTest.java
@@ -1,70 +1,35 @@
 package CalculatorAutomation;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.URL;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.windows.WindowsDriver;
 
 public class CalculatorTest {
-	
-	@SuppressWarnings("rawtypes")
-	private static WindowsDriver CalculatorSession = null;
-	private static WebElement  CalculatorResult = null;
 
-	@SuppressWarnings({ "rawtypes", "deprecation" })
-	@BeforeClass
-	public static void Setup()
-	{
-		try {
-			DesiredCapabilities capabilities=new DesiredCapabilities();
-			capabilities.setCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
-			CalculatorSession = new WindowsDriver(new URL("http://127.0.0.1:4723/"),capabilities);
-			CalculatorSession.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			
-			CalculatorResult=CalculatorSession.findElementByAccessibilityId("CalculatorResults");
-			Assert.assertNotNull(CalculatorResult);
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}finally {
-			
-		}
-	}
+	  @SuppressWarnings("rawtypes")
+	  static WindowsDriver calSession;
+	  @SuppressWarnings("deprecation")
 	
-	@AfterClass
-	public static void TearDown()
-	{
-		CalculatorResult = null;
-		if(CalculatorSession !=null) {
-			CalculatorSession.quit();
-			
-		}
-		CalculatorSession = null;	
-	}
-	
-	@Test
-	public void Addition()
-	{
-		CalculatorSession.findElementByName("One").click();
-		CalculatorSession.findElementByName("Plus").click();
-		CalculatorSession.findElementByName("Seven").click();
-		CalculatorSession.findElementByName("Equals").click();
-		//Assert.assertEquals("8", _GetCalculatorResultText());
-		String print=_GetCalculatorResultText();
-		System.out.println("The Value is:"+ print);
-	}
-	
-	protected String _GetCalculatorResultText() {
-		
-		return CalculatorResult.getText().replace("Display is", "").trim();
-	}
+	  public static void main(String[] args) throws Exception
+	  {
+		  DesiredCapabilities caps = new DesiredCapabilities();
+	      caps.setCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+	      calSession = new WindowsDriver<>(new URL("  http://127.0.0.1:4723/"), caps);
+	      System.out.println(calSession.getSessionId());
+	      calSession.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	      
+	      String calName = calSession.findElementByAccessibilityId("Header").getText();
+	      System.out.println(calName);
+	      
+	      calSession.findElementByName("One").click();
+	      calSession.findElementByName("Plus").click();
+	      calSession.findElementByName("Nine").click();
+	      calSession.findElementByName("Equals").click();
+	      
+	      String result = calSession.findElementByAccessibilityId("CalculatorResults").getText();
+	      System.out.println(result);
+	      calSession.findElementByName("Close Calculator").click();
+	      
+	  }
 }
-
